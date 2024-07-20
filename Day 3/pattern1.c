8bcfa732f72fccf10a0df80473bf5247bbe67768
Dwaraka Prasanth <dwaraka.prasanthk@gmail.com>
2024-07-20 20:08:56 +0530

    Create pattern1.c

---
+++ Day 3/pattern1.c
@@ -0,0 +1,23 @@
+ Write a program in C to display a pattern like a right angle triangle using an asterisk.
+
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("* ",j);
+        }
+        printf("\n");
+    }
+}
+
+/* Output
+Enter the number of rows:5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
